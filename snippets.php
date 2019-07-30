69c2c97c83fad1259fe39b0c3e157e01c430df7f
Gernard Cerma <aristech.gr@gmail.com>
2019-07-30 11:55:58 +0300

    Update and rename snipets.php to snippets.php

--- snippets.php
+++ snippets.php
@@ -72,3 +72,36 @@ function buy_one_get_one_free( $wc_cart ){
         # Note: Last argument in add_fee() method is related to applying the tax or not to the discount (true or false)
     }
 }
+
+/**
+ * @snippet       Buy 1, Get Free Product as a Gift - WooCommerce
+ * @how-to        Watch tutorial @ https://businessbloomer.com/?p=19055
+ * @sourcecode    https://businessbloomer.com/?p=21732
+ * @author        Rodolfo Melogli
+ * @compatible    Woo 3.5.3
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+ 
+add_filter( 'woocommerce_add_to_cart_validation', 'bbloomer_add_gift_if_sku_added_cart', 10, 3 );
+ 
+function bbloomer_add_gift_if_sku_added_cart( $passed, $product_id, $quantity ) {
+global $woocommerce;
+ 
+/* enter array of SKUs that give gift */
+$skuswithgift = array('armpar','armsup');
+ 
+/* enter SKU of product gift */
+$giftsku = 'starter';
+ 
+/* enter name of coupon that gives 100% discount for specific product gift */
+$coupon_code = 'xxxyyyzzz'; 
+ 
+$product = wc_get_product( $product_id );
+ 
+if ( $product->get_sku() && in_array( $product->get_sku(), $skuswithgift ) ) {
+   WC()->cart->add_to_cart( wc_get_product_id_by_sku($giftsku) );
+   wc_add_notice( __( 'Hey, Supporter! As promised, I added #CustomizeWoo Video Lessons for Free - you will be able to access them in a single place (~ My Courses ~) and get premium support.', 'woocommerce' ), 'success' );
+   $woocommerce->cart->add_discount( $coupon_code );
+}
+return $passed;
+}
